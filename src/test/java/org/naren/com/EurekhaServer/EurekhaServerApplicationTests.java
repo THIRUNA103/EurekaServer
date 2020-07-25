4772fa74248cc9522d8c9ed07319b1cf810d1b0e
Narendar <thirumandas@gmail.com>
2020-07-26 01:03:37 +0530

    server chnages

---
+++ src/test/java/org/naren/com/EurekhaServer/EurekhaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.naren.com.EurekhaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekhaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
